52f8fac006021117e580863ba686daa7e260d672
Marcin Puc <marcin.e.puc@gmail.com>
2019-01-17 22:18:42 +0100

    Fix multiprocess test

--- utils/manual-tests/scripts/multiprocessing_test.sh
+++ utils/manual-tests/scripts/multiprocessing_test.sh
@@ -6,23 +6,34 @@ TEST_FILES_PATH="$PROJECT_ROOT/utils/manual-tests/test-files"
 PATH="$TEST_FILES_PATH:$PATH"
 cd "$TEST_FILES_PATH"
 
+NUM_LS_INVOCATIONS=100
+
 function concurrent_ls() {
-    for i in $(seq 10); do
-    ls -f test.mdofs "/" >/dev/null &
+    for i in "$(seq ${NUM_LS_INVOCATIONS})"; do
+        ls -f "test.mdofs" "/" >/dev/null &
     done
+
+    wait
 }
 
 echo "Testing concurrent ls and mkdir"
 
 init -f test.mdofs >/dev/null 2>&1
+
 concurrent_ls &
-if mkdir -f test.mdofs "/dir";
+mkdir -f test.mdofs "/dir" >/dev/null 2>&1 &
+MKDIR_PID="$!"
+
+wait "$MKDIR_PID"
+MKDIR_EXIT_CODE="$?"
+
+wait
+
+if [[ ${MKDIR_EXIT_CODE} -eq 0 ]]
 then
     echo "Passed"
 else
     echo "Failed"
 fi
-
-ls -f test.mdofs "/"
 
 rm -f test.mdofs
