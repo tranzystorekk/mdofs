b1424f7a337b41092854aca9caf2509920d71eb9
Marcin Puc <marcin.e.puc@gmail.com>
2019-01-19 19:21:52 +0100

    Add empty unlink() implementation

---
+++ simplefs/code/include/unlink.h
@@ -0,0 +1,10 @@
+#ifndef MDOFS_UNLINK_H
+#define MDOFS_UNLINK_H
+
+namespace simplefs {
+
+int unlink(const char* name);
+
+} // simplefs
+
+#endif //MDOFS_UNLINK_H
---
+++ simplefs/code/src/unlink.cpp
@@ -0,0 +1,5 @@
+#include "unlink.h"
+
+int simplefs::unlink(const char* name) {
+    return 0;
+}
