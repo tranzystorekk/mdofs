cf77cc7a46a25a21c2426b5e6217e2aeb4a77e30
Marcin Puc <marcin.e.puc@gmail.com>
2019-01-19 18:11:41 +0100

    Add read() API function

---
+++ simplefs/code/include/read.h
@@ -0,0 +1,10 @@
+#ifndef MDOFS_READ_H
+#define MDOFS_READ_H
+
+namespace simplefs {
+
+int read(int fd, char* buf, int len);
+
+} // simplefs
+
+#endif //MDOFS_READ_H
---
+++ simplefs/code/src/read.cpp
@@ -0,0 +1,48 @@
+#include "read.h"
+
+#include <unistd.h>
+
+#include "common.h"
+
+int simplefs::read(int fd, char* buf, int len) {
+    if (buf == nullptr) {
+        // TODO error
+        return -1;
+    }
+
+    if (len < 0) {
+        // TODO error
+        return -1;
+    } else if (len == 0) {
+        return 0;
+    }
+
+    // check if desciptor index is valid
+    if (fd < 0 || fd >= simplefs::MAX_FILE_DESCRIPTORS) {
+        // TODO error
+        return -1;
+    }
+
+    simplefs::FileDescriptor& fileDescriptor = simplefs::FdTable[fd];
+
+    // check if file descriptor is actually open
+    if (fileDescriptor.is_free) {
+        // TODO error
+        return -1;
+    }
+
+    fsproto::Inode& fileNode = *simplefs::Inodes.mutable_inodes(fileDescriptor.inode);
+
+    unsigned int readableLength = len;
+    const unsigned int fileSize = fileNode.size();
+    if (fileDescriptor.fpos + readableLength > fileSize) {
+        readableLength = fileSize - fileDescriptor.fpos;
+    }
+
+    lseek(FsHandle, simplefs::Inodes.filesystem_origin() + fileNode.origin() + fileDescriptor.fpos, SEEK_SET);
+    ::read(FsHandle, buf, readableLength);
+
+    fileDescriptor.fpos += readableLength;
+
+    return readableLength;
+}
