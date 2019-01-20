5747222176225ad87a9b7603ad2f74c623e63f12
Grzegorz Nowacki <grzegorznowack@gmail.com>
2019-01-20 19:07:07 +0100

    Fix multiprocessing tests

--- utils/manual-tests/scripts/multiprocessing_test.sh
+++ utils/manual-tests/scripts/multiprocessing_test.sh
@@ -6,10 +6,10 @@ source env_vars.sh
 PATH="$BIN_PATH:$PATH"
 cd "$TEST_FILES_PATH" || exit 1
 
-NUM_LS_INVOCATIONS=100
+NUM_LS_INVOCATIONS=10000
 
 function concurrent_ls() {
-    for i in "$(seq ${NUM_LS_INVOCATIONS})"; do
+    for i in $(seq "${NUM_LS_INVOCATIONS}"); do
         ls -f "test.mdofs" "/" >/dev/null &
     done
 
@@ -37,3 +37,50 @@ else
 fi
 
 /usr/bin/rm -f test.mdofs
+
+###################################################################
+
+function concurrent_cat() {
+    pids=()
+    for i in $(seq "${NUM_LS_INVOCATIONS}"); do
+        cat -f "test.mdofs" "/file1" >/dev/null &
+        pids+=("$!")
+    done
+
+#    wait
+    echo "${pids[*]}"
+    for p in ${pids[@]}; do
+            if wait "$p"; then
+                    echo "Process $p success"
+            else
+                    echo "Process $p fail"
+            fi
+    done
+}
+
+echo "Testing concurrent cat and rm"
+
+init -f test.mdofs >/dev/null 2>&1
+
+mkfile -f test.mdofs "/file1" >/dev/null 2>&1
+
+concurrent_cat &
+
+sleep 1
+rm -f test.mdofs "/file1" >/dev/null 2>&1 &
+RM_PID="$!"
+
+wait "$RM_PID"
+RM_EXIT_CODE="$?"
+
+wait
+
+if [[ ${RM_EXIT_CODE} -eq 0 ]]
+then
+    echo "Passed"
+else
+    echo "Failed"
+fi
+
+/usr/bin/rm -f test.mdofs
+
